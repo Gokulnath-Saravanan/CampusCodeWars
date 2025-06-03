4cc9a31a323268f38abec9f1b412b9e588e1e69b
Gokulnath S <9036956408.gn@gmail>
2025-06-03 11:36:58 +0530

    fix: update Jest and Babel config for ESM modules, add axios mock

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,1 +1,17 @@
- 
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      targets: { node: 'current' },
+      modules: 'commonjs'
+    }],
+    '@babel/preset-typescript',
+    ['@babel/preset-react', { runtime: 'automatic' }]
+  ],
+  plugins: [
+    ['@babel/plugin-transform-modules-commonjs', {
+      allowTopLevelThis: true,
+      loose: true,
+      strict: false
+    }]
+  ]
+}; 
--- client/jest.config.js
+++ client/jest.config.js
@@ -4,17 +4,20 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^axios$': require.resolve('axios')
   },
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(js|jsx|mjs)$': ['babel-jest', { configFile: './babel.config.js' }]
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!axios|@mui|@emotion|@monaco-editor)/'
+    'node_modules/(?!axios|@mui|@emotion|@monaco-editor)/'
   ],
   globals: {
     'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.json'
+      tsconfig: '<rootDir>/tsconfig.json',
+      useESM: true
     }
-  }
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx']
 }; 
